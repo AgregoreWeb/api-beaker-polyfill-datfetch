7b7bcb528841edfe3c037cb682a82f37c20b041a
Kyran <kyran@gostelow.me>
2020-10-28 15:43:10 +1100

    X-Resolve

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-let altFetch
-if(/.*(BeakerBrowser).*/g.test(navigator.userAgent)) altFetch = (...args) => {
-	if(args[1].method === 'PUT') return beaker.hyperdrive.writeFile(args[0], args[1].body) // TODO: Return fake response
-	return fetch(...args)
-}
-else altFetch = window.fetch
-
-export default altFetch
+export default
+	!(/.*(BeakerBrowser).*/g.test(navigator.userAgent))
+	? window.fetch
+	: async (
+		...args
+	) => {
+		if(args[1]) {
+			if(args[1].method) {
+				if(args[1].method === 'PUT') {
+					await beaker.hyperdrive.writeFile(args[0], args[1].body)
+					return new Response() // TODO: Improve fake response
+				}
+			} 
+			if(args[1].headers) {
+				if(args[1].headers['X-Resolve'] === 'none') {
+					let [drive, path] = (args[0] + (args[0].endsWith('/') ? '' : '/') + '*').replace(/^.*:\/\/([^\/]*)(.*)$/i, '$1,$2').split(',')
+					let queryResult = await beaker.hyperdrive.query({drive: drive, path: path})
+					return new Response(JSON.stringify(queryResult.map(item => item.path.replace(/^.*\/([^\/]*)$/i, '$1') + (item.stat.isDirectory() ? '/' : ''))))
+					// TODO: Improve fake response
+					// TODO: Non-JSON output
+					// TODO: Clean
+				}
+			}
+		}
+		return window.fetch(...args)
+	}
